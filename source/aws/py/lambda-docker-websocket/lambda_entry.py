b1ee2590b817b516557fe11d82b22572497b82eb
Zhenyu Shi <errisy@gmail.com>
2022-02-20 01:02:07 -0800

    Anonymous Commit

--- source/aws/py/lambda-docker-websocket/lambda_entry.py
+++ source/aws/py/lambda-docker-websocket/lambda_entry.py
@@ -1,5 +1,6 @@
 # use the '../.env' environment variables for local debugging
 import use_env
+import json
 import asyncio
 from typing import Dict
 from UniRpc.LambdaWebsocketTypes import IWebsocketEvent
@@ -10,7 +11,9 @@ async def websocket_handler(event: IWebsocketEvent):
     websocketService: WebsocketService = WebsocketService()
     # register your service implementations here
     # websocketService.RegisterService(Namespace.ServiceImpl())
-    return await websocketService.ProcessEvent(event)
+    response = await websocketService.ProcessEvent(event)
+    print('Function Response:', json.dumps(response))
+    return response
 
 
 def handler(event: IWebsocketEvent, context: Dict[str, any] = None):
